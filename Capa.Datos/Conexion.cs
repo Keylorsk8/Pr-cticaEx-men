c0b0c05bf6469bc2809e5c3b0c2488bf5e53a631
KeylorSk8 <KeylorSk8@KEYLORSK8>
2018-08-06 20:07:54 -0600

    División en Capas

---
+++ Capa.Datos/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa.Datos
+{
+    public class Conexion
+    {
+        public static string Cadena
+        {
+            get
+            {
+                string nombre = "Default";
+                return System.Configuration.ConfigurationManager.ConnectionStrings[nombre].ConnectionString;
+            }
+        }
+    }
+}
